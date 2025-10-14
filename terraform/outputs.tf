8229e0cf142fbaccc2e007ce66311dcef783b97a
Ali anjo <alianjo178@gmail.com>
2025-10-15 00:04:38 +0330

    Fix Output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,11 @@
 output "lambda_function_arn" {
-  value = aws_lambda_function.ec2_backup_function.arn
+  value = aws_lambda_function.ec2_backup.arn
 }
 
 output "eventbridge_rule_arn" {
-  value = aws_cloudwatch_event_rule.ec2_backup_schedule.arn
+  value = aws_cloudwatch_event_rule.schedule.arn
 }
 
 output "iam_role_arn" {
-  value = aws_iam_role.lambda_execution_role.arn
+  value = aws_iam_role.lambda_exec.arn
 }
