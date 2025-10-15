b106b096b77e32d9fab711cd39a6efe2cb0e4c74
Ali anjo <alianjo178@gmail.com>
2025-10-15 22:42:48 +0330

    Fix Versions

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,9 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 3.76.1"
     }
   }
 
   required_version = ">= 0.12"
+}
+
+provider "aws" {
+  region = var.aws_region
 }
